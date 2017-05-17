fa2f9bc73682a25b44ad5f3d7375bdd12e380d85
dd di cesare <didi@posteo.net>
2017-05-17 16:45:57 +0200

    [base] quick example displaying spec with vue-openapi

---
+++ App.vue
@@ -0,0 +1,22 @@
+<template>
+    <div id="app">
+        <link rel="stylesheet" href="//fonts.googleapis.com/css?family=Roboto:300,400,500,700,400italic">
+        <link rel="stylesheet" href="//fonts.googleapis.com/icon?family=Material+Icons">
+        <open-api v-if="spec" :api="spec"></open-api>
+    </div>
+</template>
+
+<script>
+  import { mapGetters } from 'vuex'
+  import OpenApi from 'vue-openapi'
+
+  export default {
+    computed: mapGetters([
+      'spec'
+    ]),
+    components: {
+      OpenApi
+    }
+  }
+</script>
+
--- index.js
+++ index.js
@@ -1,11 +1,45 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import VueMaterial from 'vue-material'
+import 'vue-material/dist/vue-material.css'
+import VueResource from 'vue-resource'
+import App from './App.vue'
 
+Vue.use(VueMaterial)
+Vue.use(VueResource)
 Vue.use(Vuex)
 
-new Vue({
-  el: '#app',
-  render() {
-    return <h1>Hello Vue.</h1>
+Vue.material.registerTheme({
+  default: {
+    primary: 'blue',
+    accent: 'red'
+  },
+  green: {
+    primary: 'green',
+    accent: 'pink'
+  },
+  orange: {
+    primary: 'orange',
+    accent: 'green'
+  },
+})
+
+import jsonApi from './anapi.json'
+
+const store = new Vuex.Store({
+  state: {
+    spec: jsonApi
+  },
+  getters: {
+    spec: state => state.spec
   }
 })
+
+const app = new Vue({
+  el: '#app',
+  store,
+  render: h => h(App)
+
+})
+
+console.log(store, app)
